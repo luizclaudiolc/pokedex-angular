1f0fb0cbe8024e248c33062d8516eb96a9323b6c
Luiz Claudio <luiz.claudiolc@outlook.com>
2022-03-08 20:36:57 -0300

    frist commit

---
+++ src/app/pokemon-card/pokemon-card.component.ts
@@ -0,0 +1,31 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemon-card',
+  templateUrl: './pokemon-card.component.html',
+  styleUrls: ['./pokemon-card.component.scss']
+})
+export class PokemonCardComponent implements OnInit {
+  @Input() pokemon?: string;
+  @Input() pokemonIndex?: number;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    console.log(this.pokemonIndex);
+  }
+
+  getPokemonImageUrl(): string {
+    const pokemonIndex = this.leadingZero(this.pokemonIndex as number);
+    return `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${pokemonIndex}.png `
+  }
+
+  leadingZero(str: string | number, size = 3): string {
+    let s = String(str);
+    while (s.length < (size || 2)) {
+      s = `0${s}`;
+    }
+    return s;
+  }
+
+}
---
+++ src/app/pokemon-list/pokemon-list.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemon-list',
+  templateUrl: './pokemon-list.component.html',
+  styleUrls: ['./pokemon-list.component.scss'],
+})
+export class PokemonListComponent implements OnInit {
+  constructor() {}
+
+  pokemonList: Array<any> = [
+    'Bulbasaur',
+    'Yvysaur',
+    'Venusaur',
+    'Charmander',
+    'Charmeleon',
+    'Charizard',
+    'Squirtle',
+    'Wartortle',
+    'Blastoise',
+    'Caterpie',
+  ];
+
+  ngOnInit(): void {}
+}
